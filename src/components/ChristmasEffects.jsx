70e4e2d812797b72292f0d361806d030e0ccb191
Anoop Tripathi <anooptri16@gmail.com>
2025-12-14 15:10:29 +0530

    define react

--- src/components/ChristmasEffects.jsx
+++ src/components/ChristmasEffects.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import Snowfall from "react-snowfall";
 import { isDecember } from "../utils/isDecember";
 import { fireCrackers } from "../utils/fireCrackers";
